37efbe46852fdda027228cd1cc7f5011b3df093f
malakalomari <malak21alomari@gmail.com>
2019-01-19 00:59:31 +0200

    Adding ICP algo using OpenCv

--- Assets/Scripts/DetectJoints.cs
+++ Assets/Scripts/DetectJoints.cs
@@ -1,15 +1,25 @@
-﻿
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using Windows.Kinect;
 using Kinect = Windows.Kinect;
 
 
+// Define the functions which can be called from the .dll.
+internal static class OpenCVInterop
+{
+    [DllImport("Project1")]
+    internal unsafe static extern void icpGetCurrentICPModel(double[,] ModelPointsArr, double[,] DataPointsArr, double[,] transformedPointsArr);
+}
 
 public class DetectJoints : MonoBehaviour {
     public GameObject BodySrcManager;
     public JointType TrackedJoint;
+    public JointType TrackedJoint2;
+    public JointType TrackedJoint3;
+
     public JointType HandRight = JointType.HandRight;
     public JointType HandLeft = JointType.HandLeft;
     public JointType ElbowRight= JointType.ElbowRight;
@@ -21,13 +31,8 @@ public class DetectJoints : MonoBehaviour {
 	public BodySourceManager bodyManager;
     private Body[] bodies;
 	public Vector3 multiplier;
-    //Quaternion jointRotation;
-//    KinectJointOrientationFilter filter = new KinectJointOrientationFilter();
 
-    // change params if you want
     void Start() {
-
-//        filter.Init(); // change params if you want
     }
 	
 	// Update is called once per frame
@@ -50,8 +55,6 @@ public class DetectJoints : MonoBehaviour {
             if (body.IsTracked)
             {
                 //check for t-pose
-              
-//                filter.UpdateFilter(body);
                 var rh = body.Joints[HandRight].Position;
                 var lh = body.Joints[HandLeft].Position;
                 var rsh = body.Joints[ElbowRight].Position;
@@ -75,11 +78,32 @@ public class DetectJoints : MonoBehaviour {
                 if (x == 1)
                 {
                     // pass to the filter object
-//                    Windows.Kinect.Vector4 c = filter.GetFilteredRotationJoint((int)TrackedJoint);
-                    // Quaternion v = GetQuaternion(c);
 					var pos = body.Joints[TrackedJoint].Position;
-                    //    var rot = body.JointOrientations[TrackedJoint].Orientation;
-                    // jointRotation.Set(rot.X, rot.Y, rot.Z, rot.W);
+                    var pos2 = body.Joints[TrackedJoint2].Position;
+                    var pos3 = body.Joints[TrackedJoint3].Position;
+
+                    double[,] ModelPointsArr = new double[,] { { pos.X, pos.Y, pos.Z }, { pos2.X, pos2.Y, pos2.Z }, { pos3.X, pos3.Y, pos3.Z } };
+
+                    // point 1 => knee , point2 => hip right, point3 => hip left
+                    var width = pos2.X - pos.X;
+                    var hight = pos.Y - pos3.Y;
+
+                    double[,] DataPointsArr = new double[,] {
+                        { transform.position.x + (width / 2), transform.position.y, transform.position.z },
+                        { transform.position.x + (width / 2), transform.position.y + hight, transform.position.z },
+                        { transform.position.x - (width / 2), transform.position.y + hight, transform.position.z }
+                     };
+
+                    double[,] transformedPointsArr = new double[3, 3];
+
+                    OpenCVInterop.icpGetCurrentICPModel(ModelPointsArr, DataPointsArr, transformedPointsArr);
+
+                    var px1 = (float)transformedPointsArr[0, 0];
+                    var py1 = (float)transformedPointsArr[0, 1];
+                    var pz1 = (float)transformedPointsArr[0, 2];
+                    var v1 = new Vector3(px1, py1, pz1);
+
+                    // transform.position = v1;
                     Quaternion actualRotation = GetQuaternion(body.JointOrientations[TrackedJoint]);
 					float x = actualRotation.x;
 					float y = actualRotation.y;
@@ -91,10 +115,6 @@ public class DetectJoints : MonoBehaviour {
 					pitchD = Mathf.Atan2(2 * ((y * z) + (w * x)), (w * w) - (x * x) - (y * y) + (z * z)) / Mathf.PI * 180.0;
 					yawD = Mathf.Asin(2 * ((w * y) - (x * z))) / Mathf.PI * 180.0;
 					rollD = Mathf.Atan2(2 * ((x * y) + (w * z)), (w * w) + (x * x) - (y * y) - (z * z)) / Mathf.PI * 180.0;
-
-					print(pitchD);
-					print(yawD);
-					print(rollD);
 
 					transform.position = Vector3.Lerp(transform.position, new Vector3(pos.X * multiplier.x, pos.Y * multiplier.y, pos.Z * multiplier.z), Time.deltaTime * 25);
 					transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(actualRotation.x, actualRotation.y * 0.5f, actualRotation.z, actualRotation.w), Time.deltaTime * 25);
